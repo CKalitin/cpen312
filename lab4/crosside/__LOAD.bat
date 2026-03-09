fea87023e14f3e09d31bc705d15c4a60127010f1
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2026-03-09 12:41:56 -0700

    move lab4 files

--- lab4/crosside/__LOAD.bat
+++ lab4/crosside/__LOAD.bat
@@ -1,3 +1,3 @@
 @echo off
 ::This file was created automatically by CrossIDE to load a hex file using Quartus_stp.
-"C:\altera_standard\25.1std\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\_Dev\cpen312\lab4\crosside\blink.hex" | find /v "Warning (113007)"
+"C:\altera_standard\25.1std\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\_Dev\cpen312\lab4\crosside\blink.HEX" | find /v "Warning (113007)"
