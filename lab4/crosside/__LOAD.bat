a4b0561bc997ca45b3ba60c37baf64083fe4d98a
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2026-03-09 12:35:43 -0700

    munro

---
+++ lab4/crosside/__LOAD.bat
@@ -0,0 +1,3 @@
+@echo off
+::This file was created automatically by CrossIDE to load a hex file using Quartus_stp.
+"C:\altera_standard\25.1std\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\_Dev\cpen312\lab4\crosside\blink.hex" | find /v "Warning (113007)"
---
+++ lab4/crosside/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\_Dev\cpen312\lab4\crosside\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\_Dev\cpen312\lab4\crosside\blink.asm"
+echo Crosside_Action Set_Hex_File C:\_Dev\cpen312\lab4\crosside\blink.HEX
